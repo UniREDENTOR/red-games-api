52629b28087ae9e1e04c77b7b5426609e81b630b
Henrique Tatagiba <henriquectatagiba@gmail.com>
2019-05-27 11:24:31 -0300

    feat: Remover active_storage das configurações

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
@@ -54,8 +51,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  config.active_storage.service = :local
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
