91fbe8b2d721409c65bc91b6b47d61fb2a801e78
Henrique Tatagiba <henriquectatagiba@gmail.com>
2019-06-06 19:50:02 -0300

    feat: Adicionar validação se evento já expirou ao votar

--- app/models/event_score.rb
+++ app/models/event_score.rb
@@ -5,4 +5,9 @@ class EventScore < ApplicationRecord
   validates_presence_of :uuid, :amount
   validates_uniqueness_of :uuid, scope: %i[team_id event_id]
   validates_inclusion_of :amount, in: 1..5
+  validate :event_expired?
+
+  def event_expired?
+    return errors.add(:event, 'já expirou') unless event.active?
+  end
 end
