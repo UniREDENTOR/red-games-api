4f62570b3cd26d5678de73a45eea4c43bdeb02fe
Henrique Tatagiba <henriquectatagiba@gmail.com>
2019-06-13 18:58:11 -0300

    Fix: Ajustar média dos times para filtrar por evento

--- app/models/event_score.rb
+++ app/models/event_score.rb
@@ -2,6 +2,8 @@ class EventScore < ApplicationRecord
   belongs_to :team
   belongs_to :event
 
+  scope :by_event, ->(event) { where(event: event) }
+
   validates_presence_of :uuid, :amount
   validates_uniqueness_of :uuid, scope: %i[team_id event_id]
   validates_inclusion_of :amount, in: 1..5
